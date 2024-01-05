d9032a0a3875924291a5bef65dfd1afed01fdee1
Andrei Yangabishev <waffleboot@gmail.com>
2024-01-05 13:42:03 +0300

    выделить парсер

--- main_test.go
+++ main_test.go
@@ -26,24 +26,15 @@ func (s *FooSuite) TestFoo() {
 	for _, file := range s.testDataFiles() {
 		file := file
 		s.Run(filepath.Base(file), func() {
+			cmdLine, output, status := s.parseTestData(file)
 
-			a, err := txtar.ParseFile(file)
-			s.NoError(err)
-			s.Len(a.Files, 3)
-			s.Equal("input", a.Files[0].Name)
-			s.Equal("output", a.Files[1].Name)
-			s.Equal("status", a.Files[2].Name)
-
-			cmdLine := strings.Fields(string(a.Files[0].Data))
-			s.True(len(cmdLine) > 0)
-
-			output, err := exec.CommandContext(context.Background(), "./foo", cmdLine[1:]...).Output()
-			s.Equal(string(a.Files[1].Data), string(output))
+			result, err := exec.CommandContext(context.Background(), cmdLine[0], cmdLine[1:]...).Output()
+			s.Equal(output, string(result))
 			if err != nil {
 				var errExit *exec.ExitError
 				if errors.As(err, &errExit) {
 					var exp int
-					_, err := fmt.Sscanf(string(a.Files[2].Data), "%d", &exp)
+					_, err := fmt.Sscanf(status, "%d", &exp)
 					s.NoError(err)
 					s.Equal(exp, errExit.ExitCode())
 					return
@@ -58,4 +49,18 @@ func (s *FooSuite) testDataFiles() []string {
 	files, err := filepath.Glob("testdata/**/*.txt")
 	assert.NoError(s.T(), err)
 	return files
+}
+
+func (s *FooSuite) parseTestData(file string) (cmdLine []string, output, status string) {
+	a, err := txtar.ParseFile(file)
+	s.NoError(err)
+	s.Len(a.Files, 3)
+	s.Equal("input", a.Files[0].Name)
+	s.Equal("output", a.Files[1].Name)
+	s.Equal("status", a.Files[2].Name)
+
+	cmdLine = strings.Fields(string(a.Files[0].Data))
+	s.True(len(cmdLine) > 0)
+
+	return cmdLine, string(a.Files[1].Data), string(a.Files[2].Data)
 }
